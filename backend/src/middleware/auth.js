d24b117c4db92dfff043dc31f26a4d66e58944fa
Giang <tragiang080404@gmail.com>
2026-03-31 17:27:24 +0700

    test thành công API events

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,6 +1,6 @@
 // src/middleware/auth.js
 module.exports = (req, res, next) => {
-  // TODO: Sau này sẽ thêm logic verify token của Supabase ở đây
+  // TODO: Sau này sẽ thêm logic verify token JWT của Supabase ở đây
   // Hiện tại giả lập ID của một học sinh để test API (chú ý: phải là UUID chuẩn)
   req.studentId = '00000000-0000-0000-0000-000000000000'; 
   next();
